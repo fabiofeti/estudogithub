009a80e902cefb4c1fb2b419163cebe3a412626c
Fabio <fffeti@sompo.com.br>
2020-05-11 00:32:18 -0300

    mudancas 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,7 @@
 							.scrollex({
 								top:		'30vh',
 								bottom:		'30vh',
-								delay:		50,
+								delay:		300,
 								initialize:	function() { $(this).addClass('inactive'); },
 								terminate:	function() { $(this).removeClass('inactive'); },
 								enter:		function() { $(this).removeClass('inactive'); },
@@ -140,7 +140,7 @@
 						$('.main.style1')
 							.scrollex({
 								mode:		'middle',
-								delay:		10,
+								delay:		300,
 								initialize:	function() { $(this).addClass('inactive'); },
 								terminate:	function() { $(this).removeClass('inactive'); },
 								enter:		function() { $(this).removeClass('inactive'); },
